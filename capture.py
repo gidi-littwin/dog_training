05ed09d6f83210812643c454193f00e3637f7577
Gidi Littwin <gidi@cogntiv.com>
2023-01-14 18:11:08 +0200

    Gray scale movement detector

--- capture.py
+++ capture.py
@@ -10,25 +10,32 @@ cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frameHeight)
 cap.set(cv2.CAP_PROP_MODE, 150)
 
 # Set frame rate
-cap.set(cv2.CAP_PROP_FPS, 10)
-fps = int(cap.get(5))
-print("fps:", fps)
+# cap.set(cv2.CAP_PROP_FPS, 10)
+# fps = int(cap.get(5))
+# print("fps:", fps)
 
-prev_img = None
+prev_gray = None
+diff_array = list(np.ones(30)*1e8)
 while cap.isOpened():
     success, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    if prev_img is not None:
-        aa = 1
+    if prev_gray is not None:
+        diff = np.sum((gray - prev_gray)**2)/frameWidth/frameHeight
+        diff_array.append(diff)
+        diff_array.pop(0)
 
-    prev_img = img
+        diff_move_avg = np.mean(diff_array)
 
-    # if success:
-    #     cv2.rectangle(gray, (0, 0), (510, 128), (0, 255, 0), 3)
-    #     cv2.imshow("Result", gray)
-    #     if cv2.waitKey(1) & 0xFF == ord("q"):
-    #         break
+        print(f'Image diff is: {diff_move_avg}')
+
+    prev_gray = gray
+
+    if success:
+        # cv2.circle(img, (8, 8), (510, 128), (0, 255, 0), -1)
+        cv2.imshow("Result", img)
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
 
 cap.release()
 cv2.destroyAllWindows()
