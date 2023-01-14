e5d4b73579a234a5dcd6adcf130b8247c2b4c0de
Gidi Littwin <gidi@cogntiv.com>
2023-01-14 17:54:06 +0200

    Frame rate

--- capture.py
+++ capture.py
@@ -1,4 +1,6 @@
 import cv2
+import numpy as np
+from matplotlib import pyplot as plt
 
 frameWidth = 640
 frameHeight = 480
@@ -6,6 +8,11 @@ cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, frameWidth)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frameHeight)
 cap.set(cv2.CAP_PROP_MODE, 150)
+
+# Set frame rate
+cap.set(cv2.CAP_PROP_FPS, 10)
+fps = int(cap.get(5))
+print("fps:", fps)
 
 prev_img = None
 while cap.isOpened():
@@ -25,3 +32,7 @@ while cap.isOpened():
 
 cap.release()
 cv2.destroyAllWindows()
+
+plt.figure(1)
+plt.imshow(img)
+plt.show()
