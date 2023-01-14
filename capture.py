7c82a7a20e3c81cf868fa36571ef22127cbdd936
Gidi Littwin <gidi@cogntiv.com>
2023-01-14 17:47:11 +0200

    Requirements

--- capture.py
+++ capture.py
@@ -7,15 +7,21 @@ cap.set(cv2.CAP_PROP_FRAME_WIDTH, frameWidth)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frameHeight)
 cap.set(cv2.CAP_PROP_MODE, 150)
 
+prev_img = None
 while cap.isOpened():
     success, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    if success:
-        cv2.rectangle(gray, (0, 0), (510, 128), (0, 255, 0), 3)
-        cv2.imshow("Result", gray)
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            break
+    if prev_img is not None:
+        aa = 1
+
+    prev_img = img
+
+    # if success:
+    #     cv2.rectangle(gray, (0, 0), (510, 128), (0, 255, 0), 3)
+    #     cv2.imshow("Result", gray)
+    #     if cv2.waitKey(1) & 0xFF == ord("q"):
+    #         break
 
 cap.release()
 cv2.destroyAllWindows()
